e14bdf9a6aa56989123710317016090e0eda8a88
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2019-01-27 18:33:44 -0500

    Pass Args To Function

--- functions/getName.js
+++ functions/getName.js
@@ -1,6 +1,7 @@
 exports.handler = function(event, context, callback) {
+  const { name, status } = JSON.parse(event.body)
   callback(null, {
     statusCode: 200,
-    body: "Rohito Bhambhani"
+    body: `Hello ${name}. I know you are ${status}`
   })
 }
