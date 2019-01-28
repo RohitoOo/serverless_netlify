48cafbbbb548d8eedea0d162f706a8a78fcb6717
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2019-01-27 19:32:27 -0500

    Netlify Lambda

--- functions/getName.js
+++ functions/getName.js
@@ -1,15 +1,21 @@
 const axios = require("axios")
 
 exports.handler = function(event, context, callback) {
-  const API_URL = "https://api.github.com/users"
-  const API_CLIENT_ID = "cb9ccbb3555f288738b6"
-  const API_CLIENT_SECRET = "fd7182028baa30228cd4cfe6fa3b23624cf18293"
+  const { API_URL, API_CLIENT_ID, API_CLIENT_SECRET } = process.env
+  // const API_URL = "https://api.github.com/users"
+  // const API_CLIENT_ID = "cb9ccbb3555f288738b6"
+  // const API_CLIENT_SECRET = "fd7182028baa30228cd4cfe6fa3b23624cf18293"
 
   const URL = `${API_URL}?client_id=${API_CLIENT_ID}&client_secret=${API_CLIENT_SECRET}`
 
   // Send User Response
   const send = body => {
     callback(null, {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers":
+          "Origin, X-Requested-With, Content-Type, Accept"
+      },
       statusCode: 200,
       body: JSON.stringify(body)
     })
