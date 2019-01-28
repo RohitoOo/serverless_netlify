c5211f59435ab6cd6d452343c0705a977716071f
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2019-01-27 19:03:48 -0500

    Get Request

--- functions/getName.js
+++ functions/getName.js
@@ -1,7 +1,29 @@
+const axios = require("axios")
+
 exports.handler = function(event, context, callback) {
-  const { name, status } = JSON.parse(event.body)
-  callback(null, {
-    statusCode: 200,
-    body: `Hello ${name}. I know you are ${status}`
-  })
+  const API_URL = "https://api.github.com/users"
+  const API_CLIENT_ID = "cb9ccbb3555f288738b6"
+  const API_CLIENT_SECRET = "fd7182028baa30228cd4cfe6fa3b23624cf18293"
+
+  const URL = `${API_URL}?client_id=${API_CLIENT_ID}&client_secret=${API_CLIENT_SECRET}`
+
+  // Send User Response
+  const send = body => {
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(body)
+    })
+  }
+
+  //Perform API call
+  const getUsers = async () => {
+    axios.get(URL).then(res => send(res.data))
+  }
+
+  if (event.httpMethod == "GET") {
+    getUsers()
+  }
+
+  // // Send User Response
+  // send(getUsers.data)
 }
